64236380dd17e89e215b58b8943cf9df364ef9fc
morilucas <lucaskmori@gmail.com>
2024-10-27 20:00:02 -0400

    debugging together api

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,13 @@
 // api/chat.js
 
 export default async function handler(req, res) {
+    if (req.method === 'OPTIONS') {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method not allowed' });
     }
